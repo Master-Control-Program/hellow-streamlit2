e916a01bbb05ebe0c1960cf974e5a166db99bb95
Brad Meadows <bradley.meadows@cnoinc.com>
2024-02-13 09:27:24 -0500

    sd

--- pages/3_Products.py
+++ pages/3_Products.py
@@ -17,7 +17,7 @@ import sqlite3
 import pandas as pd
 
 # Path to your SQLite database
-DB_PATH = "C:\\sqllite\\brad1.db"
+DB_PATH = "C:\\SQLLITE\\brad1.db"
 
 def connect_to_db(path):
     """Connect to the SQLite database."""
