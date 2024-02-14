1654f9f56315019213049bea7e7b3c11ffb89be2
Brad Meadows <bradley.meadows@cnoinc.com>
2024-02-13 19:23:05 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,7 +18,7 @@ import streamlit as st
 import sqlite3
 
 # Custom CSS to style the title
-st.markdown("<h1 style='text-align: center; color: black; font-size: 36px; font-weight: bold;'>Coverage</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white; font-size: 52px; font-weight: bold;'>Coverage</h1>", unsafe_allow_html=True)
 
 # Create a section for "Search Criteria"
 st.header("Search Criteria")
